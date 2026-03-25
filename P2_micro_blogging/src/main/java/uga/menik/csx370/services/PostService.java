c6608bf4414e25689f18e451ce06ee8121f696f9
MellyDaG <166264920+MellyDaG@users.noreply.github.com>
2026-03-25 19:59:31 -0400

    Update PostService.java

--- P2_micro_blogging/src/main/java/uga/menik/csx370/services/PostService.java
+++ P2_micro_blogging/src/main/java/uga/menik/csx370/services/PostService.java
@@ -371,6 +371,24 @@ public class PostService {
         	return true;
     	}
 
+		 public boolean addHeart(String userId, String postId) {
+            if (isHearted(postId, userId, dataSource)) {
+                return true;
+            }
+            final String sql = "INSERT INTO heart (userId, postId) VALUES (?, ?)";
+            
+            try (Connection conn = data.Source.getConnection();
+                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                
+                pstmt.setString(1, userId);
+                pstmt.setString(2, postId);
+                
+                return pstmt.executeUpdate() == 1;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false;
+        }
+
     final String sql = "DELETE FROM bookmark WHERE userId = ? AND postId = ?";
 
     try (Connection conn = dataSource.getConnection();
